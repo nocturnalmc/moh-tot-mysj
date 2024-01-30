17f7e255890287117080429985fe491932307f9d
noct <ezy_95@outlook.com>
2024-01-30 10:32:07 +0800

    feat: add schedule appt module

---
+++ myclinic01/src/app/appointments/add-or-schedule-appointment/add-or-schedule-appointment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-or-schedule-appointment',
+  templateUrl: './add-or-schedule-appointment.component.html',
+  styleUrl: './add-or-schedule-appointment.component.scss'
+})
+export class AddOrScheduleAppointmentComponent {
+
+}
