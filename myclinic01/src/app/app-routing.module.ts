f8f5cdd59b4456495cb9b6cf179f80fa9a7d0674
noct <ezy_95@outlook.com>
2024-01-30 09:46:30 +0800

    feat: init appt routing

--- myclinic01/src/app/app-routing.module.ts
+++ myclinic01/src/app/app-routing.module.ts
@@ -17,7 +17,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
-
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
@@ -25,8 +24,14 @@ const routes: Routes = [
     path: 'view',
     loadChildren: () => import('./view/view.module').then((m) => m.ViewModule),
   },
+  {
+    path: 'appointments',
+    loadChildren: () =>
+      import('./appointments/appointments.module').then(
+        (m) => m.AppointmentsModule
+      ),
+  },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- myclinic01/src/app/appointments/appointments-routing.module.ts
+++ myclinic01/src/app/appointments/appointments-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { AppointmentsComponent } from './appointments.component';
+import { AppointmentRecordsComponent } from './appointment-records/appointment-records.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: AppointmentsComponent,
+    children: [
+      {
+        path: 'appointment-records',
+        component: AppointmentRecordsComponent,
+      },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
